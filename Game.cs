8999c306f5b23ddeb33347d4bdf80ad9b44c1011
Nicholas Jordan <NicholasPhJordan@gmail.com>
2020-08-17 16:26:08 -0500

    Added player stats and print new player name and health

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +8,18 @@ namespace HelloWorld
     {
         public void Run()
         {
+            float health = 100.0f;
+            float healthRegen = 20;
+            string name = Console.ReadLine();
+            int Maxlevel = 100;
+            int level = Maxlevel;
+            bool ready = false;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.WriteLine("You just healed!! " + healthRegen);
+            Console.WriteLine(name + " just healed " + healthRegen);
+
+            //i miss learning this
 
         }
     }
