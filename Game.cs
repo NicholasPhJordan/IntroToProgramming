39dc588d0729f163cfa21d8b4e8497df00b72393
Nicholas Jordan <NicholasPhJordan@gmail.com>
2020-08-19 15:52:05 -0500

    Continues story and starts first interaction

--- Game.cs
+++ Game.cs
@@ -56,10 +56,35 @@ namespace HelloWorld
             else
             {
                 Console.WriteLine("Invalid input. Default Settings used.");
+                role = "Adventurer";
+                health = 90.0f;
+                damage = 12.0f;
             }
 
             //Confirms character info
-            Console.WriteLine("\nNice to meet you " + name + ". " + "The Mighty " + role + ". " + "You shall start with " + health + " health at level " + level + ". Good Luck!");
+            Console.WriteLine("\nNice to meet you " + name + ". " + "The Mighty " + role + ". " + "You shall start with " + health + " health at level " + level + "." );
+
+            //Intro
+            Console.WriteLine("You are a mighty adventurer in unknown lands where monsters, theives, and many other dangers live. " +
+                "But there is also untold treasure and glory hidden on the path ahead. Like many adventurers before you, you take on the deadly task of exploring " +
+                "these unknown lands. Good Luck!");
+
+            //Continues Adventure 
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+
+            //First Interaction
+            Console.WriteLine("You begain your walk and not long later run into a little girl in a little red hood. " +
+                "She looks up at you and smiles. Holding out her basket, she offers you an apple");
+            Console.WriteLine("Press 1 to question her");
+            Console.WriteLine("Press 2 to take an apple");
+            Console.WriteLine("Press 3 to attack");
+            char input = Console.ReadKey().KeyChar;
+            if (input == '1')
+            {
+                Console.WriteLine("You ask the little girl ");
+            }
 
             //i miss learning this
             //Always comment your code
